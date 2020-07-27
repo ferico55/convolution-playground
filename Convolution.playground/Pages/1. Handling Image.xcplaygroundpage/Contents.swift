e1ce192fdbe866d5b859ac200763517fd501c471
Ferico Samuel <ferico55@gmail.com>
2020-07-27 16:12:38 +0700

    initial image reading

---
+++ Convolution.playground/Pages/1. Handling Image.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let image = UIImage(named: "lenna.png")!
+let cgImage = image.cgImage!
+
+let width = Int(image.size.width)
+let height = Int(image.size.height)
+
+let bytesPerPixel = 4
+let bytesPerRow = width * 4
+let bitsPerComponent = 8
+
+var imageData = UnsafeMutablePointer<UInt32>.allocate(capacity: width * height)
+let colorSpace = CGColorSpaceCreateDeviceRGB()
+let bitmapInfo = CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue
+let context = CGContext(data: imageData, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
+
+context?.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+if let resultedCGImage = context?.makeImage() {
+    let resultedImage = UIImage(cgImage: resultedCGImage)
+}
